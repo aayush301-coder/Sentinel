56da0f986305371fadec2d25b82f9d3b6c925045
aayush301-coder <aayushgarg301@gmail.com>
2026-04-08 17:24:07 +0530

    Updated Time UI

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let tickingInterval;
 const button = document.getElementById("calcBtn");
 const resultDiv = document.getElementById("result");
 const tickerDiv = document.getElementById("ticker");
+const statusDiv = document.getElementById("status");
 
 button.addEventListener("click", function () {
     const budget = document.getElementById("budget").valueAsNumber;
@@ -27,28 +28,54 @@ button.addEventListener("click", function () {
         return;
     }
 
-    const totalSec = budget / burnRate;
-
-    const days = Math.floor(totalSec / 86400);
-    const hours = Math.floor((totalSec % 86400) / 3600);
-    const minutes = Math.floor((totalSec % 3600) / 60);
-
-    resultDiv.textContent = `${days} days ${hours} hours ${minutes} minutes remaining`;
-
     let currentBudget = budget;
+
+    tickerDiv.textContent = `$${currentBudget.toFixed(2)} remaining`;
+    const initialSec = currentBudget / burnRate;
+    const initDays = Math.floor(initialSec / 86400);
+    const initHours = Math.floor((initialSec % 86400) / 3600);
+    const initMinutes = Math.floor((initialSec % 3600) / 60);
+    const initSeconds = Math.floor(initialSec % 60);
+    resultDiv.textContent = `${initDays} days ${initHours} hours ${initMinutes} minutes ${initSeconds} seconds remaining`;
+
+    if(initialSec < 86400) {
+        statusDiv.textContent = `STATUS: OVERRUN RISK`;
+        document.body.classList.add("panic");
+    }
+    else {
+        document.body.classList.remove("panic");
+    }
+
 
     tickerDiv.textContent = `$${currentBudget.toFixed(2)} remaining`;
 
     tickingInterval = setInterval(() => {
-        currentBudget -= burnRate * 1;
+        currentBudget -= burnRate;
 
         if (currentBudget <= 0) {
             currentBudget = 0;
             tickerDiv.textContent = "Budget Exhausted";
+            resultDiv.textContent = "Time Finished";
             clearInterval(tickingInterval);
             return;
         }
 
         tickerDiv.textContent = `$${currentBudget.toFixed(2)} remaining`;
+
+        const remainingSec = currentBudget / burnRate;
+        const days = Math.floor(remainingSec / 86400);
+        const hours = Math.floor((remainingSec % 86400) / 3600);
+        const minutes = Math.floor((remainingSec % 3600) / 60);
+        const seconds = Math.floor(remainingSec % 60);
+
+        resultDiv.textContent = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds remaining`;
+        if(remainingSec < 86400) {
+            statusDiv.textContent = `STATUS: OVERRUN RISK`;
+            document.body.classList.add("panic");
+        } 
+        else {
+            document.body.classList.remove("panic");
+        }
+
     }, 1000);
 });
